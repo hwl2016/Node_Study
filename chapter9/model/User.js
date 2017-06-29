828612cdaf406d156c10a5f89bd9996b8a02b0c2
hwl2016 <hwl2016@sina.com>
2017-06-29 11:39:45 +0800

    Signed-off-by: hwl2016 <hwl2016@sina.com>

--- chapter9/model/User.js
+++ chapter9/model/User.js
@@ -4,83 +4,83 @@ var bcrypt = require('bcrypt-nodejs');
 var db = redis.createClient();
 
 function User(obj) {
-	for(key in obj) {
-		this[key] = obj[key];
-	}
+    for (key in obj) {
+        this[key] = obj[key];
+    }
 }
 
 User.prototype = {
-	save: function(cb) {
-		if(this.id) {	//用户已存在
-			this.update(cb);
-		}else {
-			var user = this;
-			db.incr('user:ids', (err, id) => {	//创建唯一id
-				if(err) return cb(err);
-				user.id = id;	//设定id 以便保存
-				user.hashPassword((err) => {	//密码加密
-					if(err) return cb(err);
-					user.update(cb);	//保存用户属性
-				})
-			})
-		}
-	},
-	update: function(cb) {
-		var user = this;
-		var id = user.id;
-		db.set('user:id:' + user.name, id, (err) => {	//用户名称索引用户ID
-			if(err) return cb(err);
-			db.hmset('user:' + id, user, (err) => {	//用redis哈希存储
-				cb(err);
-			});
-		})
-	},
-	hashPassword: function(cb) {
-		var user = this;
-		bcrypt.genSalt(12, (err, salt) => {	//生成12个字符的盐
-			if(err) return cb(err);
-			user.salt = salt;
-			bcrypt.hash(user.pass, salt, null, (err, hash) => {	//设定盐以便保存
-				if(err) return cb(err);
-				user.pass = hash;	//设定哈希以便保存
-				cb();
-			})
-		})
-	}
+    save: function (cb) {
+        if (this.id) {	//用户已存在
+            this.update(cb);
+        } else {
+            var user = this;
+            db.incr('user:ids', (err, id) => {	//创建唯一id
+                if (err) return cb(err);
+                user.id = id;	//设定id 以便保存
+                user.hashPassword((err) => {	//密码加密
+                    if (err) return cb(err);
+                    user.update(cb);	//保存用户属性
+                })
+            })
+        }
+    },
+    update: function (cb) {
+        var user = this;
+        var id = user.id;
+        db.set('user:id:' + user.name, id, (err) => {	//用户名称索引用户ID
+            if (err) return cb(err);
+            db.hmset('user:' + id, user, (err) => {	//用redis哈希存储
+                cb(err);
+            });
+        })
+    },
+    hashPassword: function (cb) {
+        var user = this;
+        bcrypt.genSalt(12, (err, salt) => {	//生成12个字符的盐
+            if (err) return cb(err);
+            user.salt = salt;
+            bcrypt.hash(user.pass, salt, null, (err, hash) => {	//设定盐以便保存
+                if (err) return cb(err);
+                user.pass = hash;	//设定哈希以便保存
+                cb();
+            })
+        })
+    }
 }
 
-User.getByName = function(name, cb) {
-	User.getId(name, (err, id) => {
-		if(err) return cb(err);
-		User.get(id, cb);
-	})
+User.getByName = function (name, cb) {
+    User.getId(name, (err, id) => {
+        if (err) return cb(err);
+        User.get(id, cb);
+    })
 }
 
-User.getId = function(name, cb) {
-	db.get('user:id:' + name, cb);
+User.getId = function (name, cb) {
+    db.get('user:id:' + name, cb);
 }
 
-User.get = function(id, cb) {
-	db.hgetall('user:' + id, (err, user) => {
-		if(err) return cb(err);
-		cb(null, new User(user));
-	})
+User.get = function (id, cb) {
+    db.hgetall('user:' + id, (err, user) => {
+        if (err) return cb(err);
+        cb(null, new User(user));
+    })
 }
 
-User.auth = function(name, pass, cb) {
-	User.getByName(name, (err, user) => {
-		if(err) return cb(err);
-		if(!user.id) return cb();	//用户不存在
+User.auth = function (name, pass, cb) {
+    User.getByName(name, (err, user) => {
+        if (err) return cb(err);
+        if (!user.id) return cb();	//用户不存在
 
-		//对给出的密码做hash处理
-		bcrypt.hash(pass, user.salt, null, (err, hash) => {
-			if(err) return cb(err);
-			if(hash == user.pass) {
-				return cb(null, user);
-			}
-			cb();	//密码无效
-		})
-	})
+        //对给出的密码做hash处理
+        bcrypt.hash(pass, user.salt, null, (err, hash) => {
+            if (err) return cb(err);
+            if (hash == user.pass) {
+                return cb(null, user);
+            }
+            cb();	//密码无效
+        })
+    })
 }
 
 // var tobi = new User({
--- chapter9/routes/users.js
+++ chapter9/routes/users.js
@@ -4,25 +4,23 @@ var User = require('../model/User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users');
+    res.render('users');
 });
 
 router.post('/login', function(req, res, next) {
-  var name = req.body.username;
-  var pass = req.body.password;
-  User.auth(name, pass, (err, user) => {
-  	if(err) {
-  		res.send(err)
-  	}else {
-  		res.send('login sucess')
-  	}
-  })
+    var name = req.body.username;
+    var pass = req.body.password;
+    User.auth(name, pass, (err, user) => {
+        if(err) {
+            res.send(err)
+        }else {
+            res.send('login sucess')
+        }
+    })
 });
 
 router.post('/register', function(req, res, next) {
-  res.send('register success');
+    res.send('register success');
 });
-
-
 
 module.exports = router;
