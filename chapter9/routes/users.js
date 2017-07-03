f3b93fa672f8df95d5ab8eacf1cccc8dd1435d05
hwl2016 <hwl2016@sina.com>
2017-07-03 20:38:41 +0800

    Signed-off-by: hwl2016 <hwl2016@sina.com>

--- chapter9/routes/users.js
+++ chapter9/routes/users.js
@@ -3,37 +3,37 @@ var router = express.Router();
 var User = require('../model/User');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('users');
 });
 
-router.post('/login', function(req, res, next) {
+router.post('/login', function (req, res, next) {
     var name = req.body.username;
     var pass = req.body.password;
     // console.log(res.message());
     User.auth(name, pass, (err, user) => {
-        if(err) return next(err);
+        if (err) return next(err);
 
-        if(user) {
+        if (user) {
             req.session.uid = user.id;
             res.redirect('/')
-        }else {
+        } else {
             res.error('用户名或密码错误');
             res.redirect('back');
         }
     })
 });
 
-router.post('/register', function(req, res, next) {
+router.post('/register', function (req, res, next) {
     var name = req.body.username;
     var pass = req.body.pass;
     var passagin = req.body.passagin;
-    if(pass !== passagin) {
+    if (pass !== passagin) {
         res.send('两次输入的密码不一致');
-    }else {
+    } else {
         User.getByName(name, (err, user) => {
-            if(err) return next(err);
-            if(user.id) {
+            if (err) return next(err);
+            if (user.id) {
                 res.error('用户名已存在');
                 res.redirect('back');
             } else {
@@ -41,8 +41,8 @@ router.post('/register', function(req, res, next) {
                     name: name,
                     pass: pass
                 });
-                user.save(function(err) {
-                    if(err) return next(err);
+                user.save(function (err) {
+                    if (err) return next(err);
                     req.session.uid = user.id;
                     res.redirect('/');
                 })
